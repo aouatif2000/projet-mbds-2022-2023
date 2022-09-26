4c1324c673406c79be3a84df8808a5271138bc97
Gregory Galli <greg.galli@gmail.com>
2022-09-26 09:47:05 +0200

    Ajout Sécurité

--- grails-app/init/mbds_grails_22_23/BootStrap.groovy
+++ grails-app/init/mbds_grails_22_23/BootStrap.groovy
@@ -2,11 +2,17 @@ package mbds_grails_22_23
 
 import com.mbds.grails.Annonce
 import com.mbds.grails.Illustration
+import com.mbds.grails.Role
 import com.mbds.grails.User
+import com.mbds.grails.UserRole
 
 class BootStrap {
 
     def init = { servletContext ->
+        def adminUserInstance = new User(username: "admin",password: "admin").save()
+        def adminRole = new Role(authority: "ROLE_ADMIN").save()
+        UserRole.create(adminUserInstance, adminRole, true)
+
         // On boucle sur une liste de 5 prénoms
         ["Alice", "Bob", "Charly", "Denis", "Etienne"].each {
             String username ->
